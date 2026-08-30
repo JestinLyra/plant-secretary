e7c16250dbf6cfb04b751b5e6e72f7bd3967c673
JestinLyra <wisteriamania@yahoo.com>
2026-08-31 03:09:08 +1000

    5

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='plant-secretary-v28';
-const ASSETS=['./','./index.html','./style.css?v=28','./app.js?v=28','./manifest.json?v=28','./icon.svg?v=28'];
+const CACHE='plant-secretary-v29';
+const ASSETS=['./','./index.html','./style.css?v=29','./app.js?v=29','./manifest.json?v=29','./icon.svg?v=29'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r;}).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
