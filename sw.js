236ac4b321f2a30bee2bedab821cc380e89ec443
JestinLyra <wisteriamania@yahoo.com>
2026-08-31 04:22:19 +1000

    3

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='plant-secretary-v33';
+const CACHE='plant-secretary-v34';
 const ASSETS=['./','./index.html','./style.css?v=33','./app.js?v=33','./manifest.json?v=33','./icon.svg?v=33'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
