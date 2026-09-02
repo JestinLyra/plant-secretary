063b84caff5e17fed1a87877c594723749e19815
JestinLyra <wisteriamania@yahoo.com>
2026-09-02 20:30:19 +1000

    8

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE='plant-secretary-v91';
+const CACHE='plant-secretary-v92';
 const ASSETS=[
   './',
   './index.html',
-  './style.css?v=88',
-  './app.js?v=88',
+  './style.css?v=92',
+  './app.js?v=92',
   './manifest.json',
   './icon.svg',
   'assets/icons/calendar-droplet-approved.png',
@@ -11,8 +11,8 @@ const ASSETS=[
   'assets/icons/home-leaf-circle-approved.png',
   'assets/icons/undo-watering-approved.png'
   ,'assets/icons/direct-sun-pencil.png'
-  ,'assets/icons/indirect-sun-pencil.jpeg'
-  ,'assets/icons/minimal-sun-pencil.jpeg'
+  ,'assets/icons/indirect-sun-pencil.png'
+  ,'assets/icons/minimal-sun-pencil.png'
 ];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
