a525cc2581f1bbe45730bc97bec03b3da8a20f57
JestinLyra <wisteriamania@yahoo.com>
2026-09-07 11:26:46 +1000

    Refresh cache for sunlight artwork sizing

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE='plant-secretary-v6';
+const CACHE='plant-secretary-v7';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg','./home-art.css','./home-art.js','./assets/home-sunlight.webp','./assets/home-ph.webp','./assets/home-controls.webp'];
 const decorate=async response=>{
   const text=await response.text();
-  const html=text.replace('</head>','<link rel="stylesheet" href="home-art.css?v=6"></head>').replace('</body>','<script src="home-art.js?v=5"></script></body>');
+  const html=text.replace('</head>','<link rel="stylesheet" href="home-art.css?v=7"></head>').replace('</body>','<script src="home-art.js?v=5"></script></body>');
   return new Response(html,{status:response.status,statusText:response.statusText,headers:{'Content-Type':'text/html; charset=utf-8','Cache-Control':'no-cache'}});
 };
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
