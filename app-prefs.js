5188d95cf2e82f1a8184add991b8e82330c0ebc6
JestinLyra <wisteriamania@yahoo.com>
2026-09-09 20:58:30 +1000

    Bump Plant Secretary to v1.0.49

--- app-prefs.js
+++ app-prefs.js
@@ -1,5 +1,5 @@
 (()=>{
-  const VERSION='v1.0.48';
+  const VERSION='v1.0.49';
   const WHITE_ORCHID_ID='p14';
   const CLEANUP_KEY='plant-secretary-cleanup-white-orchid-photo-v1';
   window.PLANT_SECRETARY_VERSION=VERSION;
